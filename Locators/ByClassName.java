f4d25a7bcdf9f86cef98711c9f776da920c4d43e
Hemant Tumbare <hemant_tumbare@hotmail.com>
2019-01-06 01:51:03 +0530

    Add files via upload

---
+++ Locators/ByClassName.java
@@ -0,0 +1,35 @@
+package com.hemant.locators;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ByClassName {
+
+
+	public static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.icicibank.com/");
+		
+		WebElement popUpClose = driver.findElement(By.id("push-modal-close"));
+		popUpClose.click();
+		
+		WebElement searchBox = driver.findElement(By.className("pl-login-box"));
+		searchBox.click();
+
+	}
+
+}
