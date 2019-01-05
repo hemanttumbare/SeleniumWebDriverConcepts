edff854d65b58b39fd6f18be982b81633b65b408
Hemant Tumbare <hemant_tumbare@hotmail.com>
2019-01-05 23:35:12 +0530

    Add files via upload

---
+++ Locators/ByTagName.java
@@ -0,0 +1,32 @@
+package com.hemant.locators;
+
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ByTagName {
+
+	public static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.icicibank.com/");
+		
+		List<WebElement> listOfAnchortags = driver.findElements(By.tagName("a"));
+		System.out.println("Number of Anchour Tags : "+listOfAnchortags.size());
+	}
+
+}
