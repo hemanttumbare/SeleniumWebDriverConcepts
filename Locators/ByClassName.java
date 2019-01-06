90637afaabca99ea4e09e48d9ea058fd597a32ee
Hemant Tumbare <hemant_tumbare@hotmail.com>
2019-01-06 10:29:23 +0530

    Update ByClassName.java

--- Locators/ByClassName.java
+++ Locators/ByClassName.java
@@ -24,9 +24,11 @@ public class ByClassName {
 		
 		driver.get("https://www.icicibank.com/");
 		
+		//<div id="push-modal-close" >Later</div>
 		WebElement popUpClose = driver.findElement(By.id("push-modal-close"));
 		popUpClose.click();
 		
+		//<span display-text="Personal Banking" class="pl-login-box">Personal Banking<img class="down_arrow" alt="" src="/assets/img/down_arrow.svg"></span>
 		WebElement searchBox = driver.findElement(By.className("pl-login-box"));
 		searchBox.click();
 
